ddbf6da5fe771ce3b1a80e66e146fe36c070ba13
wiston  gonzales <53524033+Wiston-g@users.noreply.github.com>
2022-03-10 20:34:32 -0500

    crud php terminado

--- products.php
+++ products.php
@@ -5,6 +5,40 @@
   $mostrarPorductos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
   
 ?>
+
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://sbapi.bancolombia.com/v2/security/oauth-otp/oauth2/authorize?",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "accept: text/html",
+    "response_type: code",
+    "scope: Transfer-Intention:write:app",
+    "redirect_uri: http://localhost/bixxus/products.php",
+    "state: false"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+?>
+
 <div class="row row-cols-1 row-cols-md-3 g-4">
 <?php foreach($mostrarPorductos as $producto){?>
 
